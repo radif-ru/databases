58eac161bef3b86aea6304bcacd119e2a9440ea0
Radif <23radif@gmail.com>
2020-03-05 19:47:16 +0300

    correct_2 bash in task-1

--- lesson02/task-1.sh
+++ lesson02/task-1.sh
@@ -5,10 +5,13 @@
 # Установите СУБД MySQL. Создайте в домашней директории файл .my.cnf, 
 # задав в нем логин и пароль, который указывался при установке.
 
-sudo apt install mysql-server
+apt install mysql-server
 
 txt="[client]
 \nuser=root
 \npassword=root_password"
 
 echo -e $txt > ~/.my.cnf
+
+chmod u=-,g=rw,o=- ~/.my.cnf
+chown root:1000 ~/.my.cnf
